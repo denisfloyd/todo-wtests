475e2ce95a2c7f3cd645be60c86acd3935f3d1e7
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2022-07-26 21:01:00 -0300

    test: :white_check_mark: add more todo feature e2e tests

--- cypress/e2e/features/todo.cy.ts
+++ cypress/e2e/features/todo.cy.ts
@@ -28,6 +28,22 @@ describe("todo spec feature", () => {
   });
 
   it("should not be able to add a task that already exists", () => {
+    const tasks = ["Todo task 1", "Todo task 1"];
+
+    cy.visit("/");
+    cy.get("input").type(tasks[0]);
+    cy.get('[data-testid="add-button"]').click();
+
+    cy.get("input").type(tasks[1]);
+    cy.get('[data-testid="add-button"]').click();
+
+    cy.get("li").should("have.length", 1);
+    cy.get("ul>li").each((el, index) => {
+      cy.wrap(el).should("contain.text", tasks[index]);
+    });
+  });
+
+  it("should add an asyncronous todo task", () => {
     const tasks = ["Todo task 1", "Todo task 2"];
 
     cy.visit("/");
@@ -44,8 +60,6 @@ describe("todo spec feature", () => {
       cy.wrap(el).should("contain.text", tasks[index]);
     });
   });
-
-  // it.todo("should add an asyncronous todo task");
 
   // it.todo("should not add async todo task when server responded with an error");
 
