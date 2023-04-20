6cdc2ecb48f8409ebde969ce314e351c32921244
Denis Ladeira <denisladeira1@gmail.com>
2023-04-20 17:14:46 -0300

    fix: fix todo add test

--- src/features/todo/components/TodoAdd.unit.spec.tsx
+++ src/features/todo/components/TodoAdd.unit.spec.tsx
@@ -17,37 +17,34 @@ describe("Todo Add", () => {
     const handleAddTodoSpy = jest.fn((e) => e.preventDefault());
     render(<TodoAdd {...props} handleAddTodo={handleAddTodoSpy} />);
 
+    // need to wait for because userEvent works in an asyncronous way
     const input = screen.getByRole("textbox");
-    userEvent.type(input, "task");
+    await userEvent.type(input, "task");
     input.focus();
-    userEvent.keyboard("{enter}");
+    await userEvent.keyboard("{enter}");
 
-    await waitFor(() => {
-      expect(handleAddTodoSpy).toHaveBeenCalled();
-    });
+    expect(handleAddTodoSpy).toHaveBeenCalled();
   });
 
   it("should call function to add todo on Button click", async () => {
     const handleAddTodoSpy = jest.fn((e) => e.preventDefault());
     render(<TodoAdd {...props} handleAddTodo={handleAddTodoSpy} />);
 
-    userEvent.type(screen.getByRole("textbox"), "task");
-    userEvent.click(screen.getByTestId("add-button"));
+    // need to wait for because userEvent works in an asyncronous way
+    await userEvent.type(screen.getByRole("textbox"), "task");
+    await userEvent.click(screen.getByTestId("add-button"));
 
-    await waitFor(() => {
-      expect(handleAddTodoSpy).toHaveBeenCalled();
-    });
+    expect(handleAddTodoSpy).toHaveBeenCalled();
   });
 
   it("should call function to add todo asynchronously", async () => {
     const handleAsyncAddTodoSpy = jest.fn((e) => e.preventDefault());
     render(<TodoAdd {...props} handleAsyncAddTodo={handleAsyncAddTodoSpy} />);
 
-    userEvent.type(screen.getByRole("textbox"), "task");
-    userEvent.click(screen.getByTestId("add-button-async"));
+    // need to wait for because userEvent works in an asyncronous way
+    await userEvent.type(screen.getByRole("textbox"), "task");
+    await userEvent.click(screen.getByTestId("add-button-async"));
 
-    await waitFor(() => {
-      expect(handleAsyncAddTodoSpy).toHaveBeenCalled();
-    });
+    expect(handleAsyncAddTodoSpy).toHaveBeenCalled();
   });
 });
